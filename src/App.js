82e14e9b5a1ddd91a0b481a3b22d7aa18874584f
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-04-16 09:55:20 +0530

    21:30 props using functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import HelloWorld from './Components/HelloWorld'
-
+import counterExample from './Components/counterExample'
 
 function App() {
   return (
