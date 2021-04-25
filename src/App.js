5b86aefb2b8abc4993cdead6b30b651a4113d869
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-04-25 15:45:21 +0530

    39:36 Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HelloWorld from './Components/HelloWorld'
 import CounterExample from './Components/CounterExample'
 import Header from './Components/Header'
 import "tailwindcss/tailwind.css"
+import Footer from './Components/Footer'
 
 function App() {
   return (
    <div>
      <Header />
      <HelloWorld name="binod Tharu"/>
+     <Footer/>
     </div>
   );
 }
