48460c1f7ded9674443678aca6fd4b7e63b26938
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-05-01 16:02:57 +0530

    56 animated navigation menu

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,25 +1,19 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-
+import { useTransition, animated } from 'react-spring'
 
 function Navigation(){
     const [showMenu, setShowMenu] = useState(false)
-     let menu
-     let menuMask
-     if(showMenu){      //means is equal to true
-        menu = 
-        <div 
-            className = "fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-             The menu
-        </div>
-        menuMask = 
-        <div
-            className = "bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-            onClick = {() => setShowMenu(false)}
-            >
-        </div>
-     }
+
+    //the menu class    className = "fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
+    //mask class       className = "bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+    const transitions = useTransition(showMenu,null, {
+        from: { position: 'absolute',opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+      })
+
     return(
         <nav>
             <span className="text-xl">
@@ -28,9 +22,10 @@ function Navigation(){
                     onClick = {() => setShowMenu(!showMenu)}
                 />
             </span>
-            { menuMask }
- 
-            { menu }
+            { 
+            transitions.map(({item, key, props})=>
+            item && <animated.div key={key} style={props}>✌️</animated.div>)
+            }
         </nav>
     )
 }
